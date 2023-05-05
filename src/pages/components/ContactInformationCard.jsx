1e9b0faaef752d5ddf939777e0ef6fcd9b89bbed
rc0fficial <thunder9905@gmail.com>
2023-05-05 14:29:16 +0500

    update

--- src/pages/components/ContactInformationCard.jsx
+++ src/pages/components/ContactInformationCard.jsx
@@ -6,17 +6,21 @@ export const ContactInformationCard = () => {
     <div>
       <div className="relative mt-[29px]">
         <label className="relative">
-        <input
-          type="email"
-          id="email"
-          placeholder="oleo_bone@gmail.com"
-          className="block w-full text-[14px] px-4 py-2 border-[#D6D8EE] border-[1px] border-solid rounded-full focus:outline-none focus:border-blue-500 custom-border pl-10"
-        />
-        <span className="absolute top-[-10px] bg-white px-2 left-4 text-[12px] text-[#84849A]">Email Address</span>
-        
-        <div className="absolute top-0 left-0 h-full flex items-center pl-3">
-          <Mail />
-        </div>
+          <div className="">
+            <input
+              type="email"
+              id="email"
+              placeholder="oleo_bone@gmail.com"
+              className="block w-full text-[14px] px-4 py-2 h-[58px] border-[#D6D8EE] border-[1px] border-solid rounded-full focus:outline-none focus:border-blue-500 custom-border pl-20"
+            />
+            <span className="absolute top-[-10px] bg-white px-2 left-4 text-[12px] text-[#84849A]">
+              Email Address
+            </span>
+
+            <div className="absolute top-0 left-0 h-full flex items-center pl-[39px]">
+              <Mail />
+            </div>
+          </div>
         </label>
       </div>
     </div>
--- src/pages/components/icons/Mail.svg.jsx
+++ src/pages/components/icons/Mail.svg.jsx
@@ -5,7 +5,7 @@ export const Mail = () => {
     <div>
       <svg
         width="20"
-        height="20"
+        height="18"
         viewBox="0 0 20 20"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
