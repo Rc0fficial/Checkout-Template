0e41e96268bd3cb2103ac731089b4a3333400089
rc0fficial <thunder9905@gmail.com>
2023-05-30 20:54:15 +0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Navbar } from "@/components/Navbar";
-import { Checkout } from "@/components/Checkout";
+import { Navbar } from "src/components/Navbar";
+import { Checkout } from "src/components/Checkout";
 
 const inter = Inter({ subsets: ["latin"] });
 
