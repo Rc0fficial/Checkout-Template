6bb9e9724f37173664a95f40e665af8775b72181
rc0fficial <thunder9905@gmail.com>
2023-06-07 15:50:16 +0500

    update

--- src/components/thankyou page/OutputCard.jsx
+++ src/components/thankyou page/OutputCard.jsx
@@ -17,7 +17,7 @@ export const OutputCard = () => {
   return (
     <div className="mx-auto mt-[50px] border-solid border-[#D6D8EE] border-[1px] rounded-3xl py-[40px] px-[40px]">
       <div className="grid grid-cols-2 gap-[60px] ">
-        <div className="flex flex-col gap-[16px]">
+        <div className="flex flex-col flex-wrap gap-[16px]">
           <div>
             <p className="text-[#4B4E68] text-[14px] font-normal">Name:</p>
             <h3 className="text-[#000034] text-[14px] font-medium ">
@@ -42,9 +42,11 @@ export const OutputCard = () => {
           </div>
         </div>
         <div className="flex flex-col gap-[16px] ">
-          <div>
+          <div className="">
             <p className="text-[#4B4E68] text-[14px] font-normal">Email:</p>
-            <h3 className="text-[#000034] text-[14px] font-medium ">{email}</h3>
+            <h3 className="text-[#000034] text-[14px] font-medium overflow-x-auto whitespace-nowrap">
+              {email}
+            </h3>
           </div>
           <div className="max-w-[200px]">
             <p className="text-[#4B4E68] text-[14px] font-normal">
