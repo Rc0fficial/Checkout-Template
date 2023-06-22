bfaf80e3826ed2a9dadf4ef2c0264465b721ce32
rc0fficial <thunder9905@gmail.com>
2023-06-22 14:31:35 +0500

    update

--- src/components/BillingAddressCard.jsx
+++ src/components/BillingAddressCard.jsx
@@ -73,7 +73,6 @@ export  const BillingAddressCard = () => {
         {useDifferentBilling && (
           <ShippingAddressForm
             value={billingAddress}
-            
             onChange={handleBillingAddressChange}
           />
         )}
